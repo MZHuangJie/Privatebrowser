e372c10416f475737a832f0560fd01156cb57dbe
MZHuangJie <46631491+MZHuangJie@users.noreply.github.com>
2026-05-09 09:37:24 +0800

    feat: add IPC handler registration

---
+++ src/main/ipc-handlers.ts
@@ -0,0 +1,66 @@
+import { ipcMain } from 'electron';
+import { TabManager } from './tab-manager';
+import { StoreManager } from './store-manager';
+import { BrowserSettings } from '../shared/types';
+
+export function registerIpcHandlers(
+  tabManager: TabManager,
+  storeManager: StoreManager
+): void {
+  ipcMain.handle('tab:create', (_e, url?: string) => {
+    return tabManager.createTab(url);
+  });
+
+  ipcMain.handle('tab:close', (_e, tabId: string) => {
+    tabManager.closeTab(tabId);
+  });
+
+  ipcMain.handle('tab:switch', (_e, tabId: string) => {
+    tabManager.switchTab(tabId);
+  });
+
+  ipcMain.handle('tab:pin', (_e, tabId: string) => {
+    tabManager.togglePin(tabId);
+  });
+
+  ipcMain.handle('nav:go', (_e, payload: { tabId: string; url: string }) => {
+    tabManager.navigate(payload.tabId, payload.url);
+  });
+
+  ipcMain.handle('nav:back', (_e, tabId: string) => {
+    tabManager.goBack(tabId);
+  });
+
+  ipcMain.handle('nav:forward', (_e, tabId: string) => {
+    tabManager.goForward(tabId);
+  });
+
+  ipcMain.handle('nav:reload', (_e, tabId: string) => {
+    tabManager.reload(tabId);
+  });
+
+  ipcMain.handle('tabs:getAll', () => {
+    return tabManager.getAllTabs();
+  });
+
+  ipcMain.handle('settings:get', () => {
+    return storeManager.getSettings();
+  });
+
+  ipcMain.handle('settings:set', (_e, partial: Partial<BrowserSettings>) => {
+    const settings = storeManager.setSettings(partial);
+    tabManager.updateSettings(settings);
+    return settings;
+  });
+
+  ipcMain.handle('bookmarks:get', () => {
+    return storeManager.getBookmarks();
+  });
+
+  ipcMain.handle('bookmarks:add', (_e, bookmark: { title: string; url: string }) => {
+    const bookmarks = storeManager.getBookmarks();
+    bookmarks.push(bookmark);
+    storeManager.setBookmarks(bookmarks);
+    return bookmarks;
+  });
+}
